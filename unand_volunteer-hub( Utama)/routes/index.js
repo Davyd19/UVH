3a91cb86848019ef2aa4697c001a37487a9e7a48
Davyd19 <147055578+Davyd19@users.noreply.github.com>
2025-06-20 21:50:37 +0700

    Delete unand_volunteer-hub( Utama) directory

--- unand_volunteer-hub( Utama)/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController');
-
-// Rute untuk halaman utama yang menampilkan pilihan peran
-router.get('/', authController.showRolePage);
-
-module.exports = router;    
