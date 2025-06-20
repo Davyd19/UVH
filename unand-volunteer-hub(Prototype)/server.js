3c914796442bbc233ff64bfb82285edcb30d2099
Davyd19 <147055578+Davyd19@users.noreply.github.com>
2025-06-20 21:50:28 +0700

    Delete unand-volunteer-hub(Prototype) directory

--- unand-volunteer-hub(Prototype)/server.js
+++
@@ -1,9 +0,0 @@
-// server.js
-
-const app = require('./app');
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server berjalan di http://localhost:${PORT}`);
-});
